e0fb9a30aa8f2a01b5eafd21b20d0f6c39dfc0f0
Danilo <realdanilo@users.noreply.github.com>
2020-09-17 16:49:27 -0700

    fix multi filter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //Modules
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useReducer } from 'react';
 import { Switch, Route } from 'react-router-dom'
 import './App.css';
 //Components
@@ -39,38 +39,54 @@ const initialState = {
 //reducer
 const reducer = (state, action) => {
   switch (action.type) {
+    case "UPDATE":
+      return { ...state, ...action.newFilter }
     case "FILTER":
       let tempRooms = [...state.rooms]
-      console.log(action.newValue, typeof (action.newValue))
-      if (action.newValue !== "all" && typeof (action.newValue) == "string") {
-        console.log("changing type of room")
-        tempRooms = tempRooms.filter(room => room.type == action.newValue);
+      let { type, capacity, price, minSize, maxSize, breakfast, pets } = state;
+      capacity = parseInt(capacity);
+      price = parseInt(price);
+      let breakfastCheck = breakfast;
+      let petsCheck = pets;
+
+      // filter by type
+      if (type !== "all") {
+        tempRooms = tempRooms.filter(room => room.type === type);
       }
-      if (action.newValue >= 0 && typeof (action.newValue) == "number") {
-        console.log("changing min # of guess")
-        tempRooms = tempRooms.filter(room => room.capacity >= action.newValue)
+      // filter by capacity
+      if (capacity !== 1) {
+        tempRooms = tempRooms.filter(room => room.capacity >= capacity);
+      }
+      // filter by price
+      tempRooms = tempRooms.filter(room => room.price <= price);
+      //filter by size
+      tempRooms = tempRooms.filter(
+        room => room.size >= minSize && room.size <= maxSize
+      );
+      //filter by breakfast
+      if (breakfast) {
+        tempRooms = tempRooms.filter(room => room.breakfast === true);
+      }
+      //filter by pets
+      if (pets) {
+        tempRooms = tempRooms.filter(room => room.pets === true);
       }
 
-      console.log(tempRooms)
-      return { ...state, [action.changeProp]: action.newValue, sortedRooms: tempRooms }
+      return { ...state, sortedRooms: tempRooms, breakfast: breakfastCheck, pets: petsCheck }
     default:
       return state
   }
 }
-function App() {
 
+
+function App() {
   // using reducer
   const [state, dispatch] = useReducer(reducer, initialState)
 
+
+  //finding and returning a room
   const getRoom = (slug) => {
-    if (state.rooms != null) {
-      return state.rooms.find(room => room.slug === slug)
-    }
-    else {
-      console.log("hitting room  = null")
-      // let backUp = formatData(items)
-      // return backUp.find(room => room.slug === slug)
-    }
+    return state.rooms.find(room => room.slug === slug)
   }
 
   return (
--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -3,18 +3,26 @@ import { RoomContext } from '../context'
 import Title from './Title'
 
 const findUnique = (items, type) => {
-    return [... new Set(items.map(item => item[type]))]
+    return [...new Set(items.map(item => item[type]))]
 }
-const handleChange = (e) => {
-    // console.log(e.target)
-    //dispatch({type:"SELECTOR", action.changeProp:e.target.name, action.newValue:e.target.value })
-}
+
 export default function RoomFilter({ rooms }) {
     const { state, dispatch } = useContext(RoomContext)
     const { type, capacity, minPrice, maxPrice, minSize, maxSize, size, price, breakfast, pets } = state
     const uniqueSelectors = findUnique(rooms, "type").concat("all")
     const people = findUnique(rooms, "capacity")
 
+    const handleChange = (e) => {
+        e.stopPropagation();
+        // targetFilter<< event
+        const target = e.target
+        const value = target.type === "checkbox" ? target.checked : target.value
+        const name = target.name
+        let newFilter = { [name]: value }
+
+        dispatch({ type: "UPDATE", newFilter })
+        dispatch({ type: "FILTER" })
+    }
 
 
     return (
@@ -24,7 +32,7 @@ export default function RoomFilter({ rooms }) {
                 {/* select type */}
                 <div className="form-group">
                     <label htmlFor="type">Room Type</label>
-                    <select name={type} id="type" value={type} className="form-control" onChange={(e) => dispatch({ type: "FILTER", changeProp: "type", newValue: e.target.value })} >
+                    <select name="type" id="type" value={type} className="form-control" onChange={(e) => handleChange(e)} >
                         {uniqueSelectors.map((opt, i) => <option key={i}>{opt}</option>)}
 
                     </select>
@@ -33,13 +41,74 @@ export default function RoomFilter({ rooms }) {
                 {/* guess */}
                 <div className="form-group">
                     <label htmlFor="capacity">Guess</label>
-                    <select name={capacity} id="capacity" value={capacity} className="form-control" onChange={(e) => dispatch({ type: "FILTER", changeProp: "capacity", newValue: parseInt(e.target.value) })} >
+                    <select name="capacity" id="capacity" value={capacity} className="form-control" onChange={(e) => handleChange(e)}>
                         {/* fix e.tar.get.value */}
                         {people.map((item, i) => <option key={i}>{item}</option>)}
 
                     </select>
                 </div>
                 {/* end of guess */}
+                {/* room price */}
+                <div className="form-group">
+                    <label htmlFor="price">Room Price ${price}</label>
+                    <input type="range"
+                        name="price"
+                        id="price"
+                        max={maxPrice}
+                        min={minPrice}
+                        value={price}
+                        onChange={(e) => handleChange(e)}
+                        className="form-control"
+                    />
+                </div>
+                {/* end of room price */}
+                {/* size */}
+                <div className="form-group">
+                    <label htmlFor="size">Room Size</label>
+                    <div className="size-inputs">
+                        <input
+                            type="number"
+                            name="minSize"
+                            min="0"
+                            value={minSize}
+                            onChange={(e) => handleChange(e)}
+                            className="size-input"
+                        />
+                        <input
+                            type="number"
+                            name="maxSize"
+                            value={maxSize}
+                            max={maxSize}
+                            onChange={(e) => handleChange(e)}
+                            className="size-input"
+                        />
+                    </div>
+                </div>
+                {/* end of size */}
+                {/* extras */}
+                <div className="form-group">
+                    <div className="single-extra">
+                        <input
+                            type="checkbox"
+                            name="breakfast"
+                            id="breakfast"
+                            checked={breakfast}
+                            onChange={(e) => handleChange(e)}
+                        />
+                        <label htmlFor="breakfast">Breakfast</label>
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="checkbox"
+                            name="pets"
+                            checked={pets}
+                            id="pets"
+                            onChange={(e) => handleChange(e)}
+                        />
+                        <label htmlFor="pets">Pets</label>
+                    </div>
+                </div>
+                {/* end of extras */}
             </form>
         </section>
     )
